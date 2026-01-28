d676c2f657bb74fe9c2efbaba2c0b1cb2a023c09
CharlesLambey <bakargacharles@gmail.com>
2026-01-28 09:47:09 +0100

    Ajoutez des fichiers projet.

---
+++ homeForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ReportCrystal
+{
+    partial class homeForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoadReport = new System.Windows.Forms.Button();
+            this.crystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLoadReport
+            // 
+            this.btnLoadReport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadReport.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnLoadReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadReport.ForeColor = System.Drawing.Color.White;
+            this.btnLoadReport.Location = new System.Drawing.Point(918, 6);
+            this.btnLoadReport.Name = "btnLoadReport";
+            this.btnLoadReport.Size = new System.Drawing.Size(180, 49);
+            this.btnLoadReport.TabIndex = 1;
+            this.btnLoadReport.Text = "Charger un Rapport";
+            this.btnLoadReport.UseVisualStyleBackColor = false;
+            this.btnLoadReport.Click += new System.EventHandler(this.btnLoadReport_Click);
+            // 
+            // crystalReportViewer
+            // 
+            this.crystalReportViewer.ActiveViewIndex = -1;
+            this.crystalReportViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.crystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer.Location = new System.Drawing.Point(2, 60);
+            this.crystalReportViewer.Name = "crystalReportViewer";
+            this.crystalReportViewer.Size = new System.Drawing.Size(1106, 581);
+            this.crystalReportViewer.TabIndex = 2;
+            // 
+            // homeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 642);
+            this.Controls.Add(this.crystalReportViewer);
+            this.Controls.Add(this.btnLoadReport);
+            this.Name = "homeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LS Reports App";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoadReport;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer;
+    }
+}
+
---
+++ homeForm.cs
@@ -0,0 +1,61 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Configuration;
+using CrystalDecisions.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportCrystal
+{
+    public partial class homeForm : Form
+    {
+        public homeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadReport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Crystal Report Files (*.rpt)|*.rpt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ReportDocument reportDocument = new ReportDocument();
+                reportDocument.Load(openFileDialog.FileName);
+
+                string serverName = ConfigurationManager.AppSettings.Get("serverName");
+                string databaseName = ConfigurationManager.AppSettings["DatabaseName"];
+                string userId = ConfigurationManager.AppSettings["UserID"];
+                string password = ConfigurationManager.AppSettings["Password"];
+
+                bool trustedConnection = bool.Parse(ConfigurationManager.AppSettings["TrustedConnection"]);
+
+                foreach (Table table in reportDocument.Database.Tables)
+                {
+                    TableLogOnInfo logOnInfo = table.LogOnInfo;
+                    logOnInfo.ConnectionInfo.ServerName = serverName;
+                    logOnInfo.ConnectionInfo.DatabaseName = databaseName;
+
+                    if (trustedConnection)
+                    {
+                        logOnInfo.ConnectionInfo.IntegratedSecurity = true;
+                    }
+                    else
+                    {
+                        logOnInfo.ConnectionInfo.UserID = ConfigurationManager.AppSettings["UserID"];
+                        logOnInfo.ConnectionInfo.Password = ConfigurationManager.AppSettings["Password"];
+                    }
+                    table.ApplyLogOnInfo(logOnInfo);
+                }
+
+                crystalReportViewer.ReportSource = reportDocument;
+            }
+        }
+    }
+}
